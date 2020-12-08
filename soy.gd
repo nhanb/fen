584483ad5c8d9b8de913a7e576b5913ad8af2313
Bùi Thành Nhân <hi@imnhan.com>
2020-12-08 18:16:36 +0700

    different animations

--- soy.gd
+++ soy.gd
@@ -3,21 +3,33 @@ extends TextureRect
 var following = false
 var dragging_start_position = Vector2()
 
+onready var fren = get_node("fren")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_tree().get_root().set_transparent_background(true)
 
 
 func _on_soy_gui_input(event):
-	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
-		if event.pressed:
-			following = true
-			dragging_start_position = get_global_mouse_position()
-		else:
-			following = false
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			if event.pressed:
+				following = true
+				dragging_start_position = get_global_mouse_position()
+				fren.play("drag")
+			else:
+				following = false
+				fren.play("idle")
+		elif event.button_index == BUTTON_RIGHT:
+			fren.play("special")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if following:
 		OS.set_window_position(OS.window_position + get_global_mouse_position() - dragging_start_position)
+
+
+func _on_fren_animation_finished():
+	if(fren.animation == "special"):
+		fren.play("idle")
